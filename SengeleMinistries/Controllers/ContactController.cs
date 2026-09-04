b56a90fd714b7702d1b2c568ae47b2ff15fcff86
Offranel <francis.sengele@gmail.com>
2026-09-03 22:58:23 -0700

    Add using directives and clarify ContactController comment
    Added using directives for required namespaces in ContactController.cs. Included a comment explaining that the Contact page is informational only and does not process form submissions. No changes to controller logic.

--- SengeleMinistries/Controllers/ContactController.cs
+++ SengeleMinistries/Controllers/ContactController.cs
@@ -1,9 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
+using SengeleMinistries.Data;
+using SengeleMinistries.Models;
+
+using Microsoft.EntityFrameworkCore;
+
 
 namespace SengeleMinistries.Controllers
 {
     public class ContactController : Controller
     {
+        // The Contact page is informational only. No form submission is handled here.
         public IActionResult Index()
         {
             return View();
