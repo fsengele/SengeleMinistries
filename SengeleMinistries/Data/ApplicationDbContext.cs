2d7b09d29c4246e2f1824fd47adc1cfd812d7491
Offranel <francis.sengele@gmail.com>
2026-09-11 22:27:48 -0700

    Add this DbSet<ShoppingCartItem> in the ApplicationDbContext

--- SengeleMinistries/Data/ApplicationDbContext.cs
+++ SengeleMinistries/Data/ApplicationDbContext.cs
@@ -22,5 +22,7 @@ namespace SengeleMinistries.Data
         public DbSet<Order> Orders { get; set; } = null!;
 
         public DbSet<OrderItem> OrderItems { get; set; } = null!;
+
+        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; } = null!;
     }
 }
