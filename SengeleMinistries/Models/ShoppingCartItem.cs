4e07f9a88f2af55172b5777f6206b925736f4a68
Offranel <francis.sengele@gmail.com>
2026-09-11 22:24:24 -0700

    Add class ShoppingCartItem to help the member to keep his cart permenatly before payment

---
+++ SengeleMinistries/Models/ShoppingCartItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SengeleMinistries.Models
+{
+    public class ShoppingCartItem
+    {
+        [Key]
+        public int ShoppingCartItemId { get; set; }
+
+        [Required]
+        public int MemberId { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+
+        [Required]
+        public int Quantity { get; set; } = 1;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
