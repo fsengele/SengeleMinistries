ccecbc6edba19d117c3eecc3a6548b6783587d4e
Offranel <francis.sengele@gmail.com>
2026-09-12 00:42:17 -0700

    Add LanguageController

---
+++ SengeleMinistries/Controllers/LanguageController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SengeleMinistries.Controllers
+{
+    public class LanguageController : Controller
+    {
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SetLanguage(
+            string culture,
+            string returnUrl)
+        {
+            // Only allow the languages supported by the website
+            if (culture != "en" && culture != "fr")
+            {
+                culture = "en";
+            }
+
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(
+                    new RequestCulture(culture)),
+                new CookieOptions
+                {
+                    Expires = DateTimeOffset.UtcNow.AddYears(1),
+                    IsEssential = true,
+                    HttpOnly = false,
+                    SameSite = SameSiteMode.Lax
+                });
+
+            if (!string.IsNullOrWhiteSpace(returnUrl) &&
+                Url.IsLocalUrl(returnUrl))
+            {
+                return LocalRedirect(returnUrl);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
