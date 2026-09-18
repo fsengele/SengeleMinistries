835e8ee79437b7f34a9b54ac562c7777e5a33ae4
Offranel <francis.sengele@gmail.com>
2026-09-17 22:56:38 -0700

    Create GivingController and View Giving index .cshtml

---
+++ SengeleMinistries/Controllers/GivingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SengeleMinistries.Controllers
+{
+    public class GivingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
